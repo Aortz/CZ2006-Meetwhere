402f67411b785de7323397df1fa38c400bee1529
Wei Jun <tiahweijun1999@gmail.com>
2022-02-26 10:56:40 +0800

    set up navigation

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AuthenticationScreen from "./screens/AuthenticationScreen/AuthenticationScreen";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import HistoryScreen from "./screens/HistoryScreen/HistoryScreen";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Authentication"
+          component={AuthenticationScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="History" component={HistoryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/AuthenticationScreen/AuthenticationScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AuthenticationScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>AuthenticationScreen</Text>
+    </View>
+  );
+};
+
+export default AuthenticationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+});
