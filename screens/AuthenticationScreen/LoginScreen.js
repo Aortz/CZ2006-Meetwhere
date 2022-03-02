e448a749095c5bd11a535196f29c17e52d0c1235
Aortz <leejunwei2@gmail.com>
2022-03-02 12:39:42 +0800

    fixed the login and signin screens

--- screens/AuthenticationScreen/LoginScreen.js
+++ screens/AuthenticationScreen/LoginScreen.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, Alert, ActivityIndicator, Pressable, Image, KeyboardAvoidingView } from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import Firebase from '../database/firebase';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 export default class Login extends Component {
   
@@ -37,7 +39,7 @@ export default class Login extends Component {
           email: '', 
           password: ''
         })
-        this.props.navigation.navigate('Dashboard')
+        this.props.navigation.navigate('Home')
       })
       .catch(error => this.setState({ errorMessage: error.message }))
     }
