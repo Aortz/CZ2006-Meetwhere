49403cea897ad99a1741449cc21e75fb1f3cfe46
bobby <cxiangyu@gmail.com>
2022-03-03 12:06:38 +0800

    edited the css fr homescreen

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -4,38 +4,40 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Welcome to MeetWhere!</Text>
-      <View style={styles.iconsview}>
-        <TouchableOpacity
-          style={styles.touchableStyle}
-          onPress={() => navigation.navigate("InputLocation")}
-        >
-          <Image
-            source={require("../../assets/random_location.png")}
-            style={styles.icon}
-          />
-        </TouchableOpacity>
+      <View style={styles.icons}>
+        <Text style={styles.header}>Welcome to MeetWhere!</Text>
+        <View style={styles.iconsview}>
+          <TouchableOpacity
+            style={styles.touchableStyle}
+            onPress={() => navigation.navigate("InputLocation")}
+          >
+            <Image
+              source={require("../../assets/random_location.png")}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.touchableStyle}
-          onPress={() => navigation.navigate("InputLocation")}
-        >
-          <Image
-            source={require("../../assets/suggested_list.png")}
-            style={styles.icon}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.touchableStyle}
+            onPress={() => navigation.navigate("InputLocation")}
+          >
+            <Image
+              source={require("../../assets/suggested_list.png")}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.touchableStyle}
-          onPress={() => navigation.navigate("History")}
-        >
-          <Image
-            source={require("../../assets/history.png")}
-            style={styles.icon}
-          />
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity
+            style={styles.touchableStyle}
+            onPress={() => navigation.navigate("History")}
+          >
+            <Image
+              source={require("../../assets/history.png")}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+        </View>
     </View>
   );
 };
@@ -50,27 +52,35 @@ const styles = StyleSheet.create({
     padding: 5,
     backgroundColor: "#fff",
     alignItems: "center",
+
   },
   icon: {
     height: 80,
     width: 80,
+  
+  },
+  icons: {
+    flexDirection: "column",
+    height: 800,
+    borderWidth: 2,
+    width: "95%",
   },
   header: {
     fontSize: 30,
     padding: 10,
     fontWeight: "bold",
     color: "black",
+    
   },
   iconsview: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 15,
-    height: 200,
-    borderWidth: 5,
-    width: "90%",
+    height:"100%",
+    width: "100%",
+    alignContent: "center",
   },
   touchableStyle: {
-    height: "60%",
+    height: "90%",
     justifyContent: "center",
   },
 });
