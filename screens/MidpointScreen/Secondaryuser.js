3178aa1feb5bd41ea427b378af8ae5b8d87b548d
bobby <cxiangyu@gmail.com>
2022-03-22 20:40:29 +0800

    fdjhjksnfk

--- screens/MidpointScreen/Secondaryuser.js
+++ screens/MidpointScreen/Secondaryuser.js
@@ -87,7 +87,7 @@ const Secondaryuser = (props) => {
             setShowFilter(true);
             setShowSecUserInput(false);
             setShowRadius(false);
-          }}
+          }}style={styles.buttonSkip}
         >
           <Text style={styles.buttonSkip}>Skip</Text>
         </TouchableOpacity>
@@ -95,13 +95,14 @@ const Secondaryuser = (props) => {
         <TouchableOpacity
           onPress={() => {
             navigation.goBack();
-          }}
+          }
+        }style={styles.buttonBack}
         >
-          <Text style={styles.buttonSkip}>Back</Text>
+          <Text style={styles.buttonBack}>Back</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={validateUser} style={styles.buttonAdd}>
-          <Text style={styles.buttonText}>Add user</Text>
+          <Text style={styles.buttonAdd}>Add user</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -113,7 +114,7 @@ export default Secondaryuser;
 const styles = StyleSheet.create({
   secondaryUser: {
     backgroundColor: "white",
-    height: "33%",
+    height: "30%",
     alignItems: "center",
     padding: 10,
     paddingHorizontal: 30,
@@ -128,7 +129,7 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    marginTop: 5,
+    marginTop: 13,
     marginBottom: 5,
     paddingLeft: 8,
     borderWidth: 1,
@@ -136,15 +137,31 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: "#DCDCDC",
   },
+
   buttonSkip: {
     textDecorationLine: "underline",
-  },
-  buttonAdd: {
-    backgroundColor: "#009688",
     borderRadius: 10,
-    paddingVertical: 10,
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+    backgroundColor: "#dcdcdc",
+  },
+
+  buttonAdd: {
+    textDecorationLine: "underline",
+    backgroundColor: "#8fbc8f",
+    borderRadius: 10,
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+  },
+  buttonBack: {
+    textDecorationLine: "underline",
+    backgroundColor: "#e9967a",
+    borderRadius: 10,
+    paddingVertical: 7,
     paddingHorizontal: 12,
   },
+
+
   buttonRow: {
     width: "90%",
     flexDirection: "row",
@@ -157,6 +174,7 @@ const styles = StyleSheet.create({
   },
   inputView: {
     width: "90%",
-    marginBottom: 13,
+    marginBottom: 15,
   },
 });
+
